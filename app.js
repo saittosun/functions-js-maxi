9f8bc692fcda851bf1cd2a1a680e95546a43190d
tostos <saittosun2@gmail.com>
2020-08-29 13:12:15 +0200

    adding user choices to the game

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 // jshint esversion: 9
 const startGameBtn = document.getElementById("start-game-btn");
 
-// declaration or statement
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! we chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
 function startGame() {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log("game is starting");
+  const playerSection = getPlayerChoice();
+  console.log(playerSection);
 }
 
-// expression (expressions were essentially the thing that yielded you values, so something you could store somewhere else)
-// const start = function startGame() {
-//   console.log("game is starting");
-// };
-
-// const person = {
-//   name: "sait",
-//   greet: function greet() {
-//     console.log("hello there");
-//   },
-// };
-
-// console.log(person.name);
-// person.greet();
-
-// I'm just passing the name here and therefore the typeof operator will have a look at the thing behind the name, so it will have a look at that thing which is stored in start game, which is our function
-console.log(typeof startGame); // function
-console.dir(startGame);
-//  a function in the end whilst having its own type is an object. It's basically also an object, a special type of object if you will with special pre-configured properties but still an object and the implication of that is for example that it's typically stored in the heap as you learned and basically anything that applies to objects, for example that they are reference types also applies to functions. Often that might not really matter to you, it's still important to be aware of that,
-
 startGameBtn.addEventListener("click", startGame);
-// startGameBtn.addEventListener("click", start);
