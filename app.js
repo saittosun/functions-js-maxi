375982bb044e8affc520a8ef474191ab45b056c5
tostos <saittosun2@gmail.com>
2020-08-29 11:38:58 +0200

    before adding user choices to the game

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // jshint esversion: 9
 const startGameBtn = document.getElementById("start-game-btn");
 
+// declaration or statement
 function startGame() {
   console.log("game is starting");
 }
+
+// expression (expressions were essentially the thing that yielded you values, so something you could store somewhere else)
+// const start = function startGame() {
+//   console.log("game is starting");
+// };
 
 // const person = {
 //   name: "sait",
@@ -16,8 +22,9 @@ function startGame() {
 // person.greet();
 
 // I'm just passing the name here and therefore the typeof operator will have a look at the thing behind the name, so it will have a look at that thing which is stored in start game, which is our function
-console.log(typeof startGame);// function
+console.log(typeof startGame); // function
 console.dir(startGame);
 //  a function in the end whilst having its own type is an object. It's basically also an object, a special type of object if you will with special pre-configured properties but still an object and the implication of that is for example that it's typically stored in the heap as you learned and basically anything that applies to objects, for example that they are reference types also applies to functions. Often that might not really matter to you, it's still important to be aware of that,
 
 startGameBtn.addEventListener("click", startGame);
+// startGameBtn.addEventListener("click", start);
