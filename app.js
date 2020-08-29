1a15e8e70ce61a03a5922f2e4c9ef8423ae4455e
tostos <saittosun2@gmail.com>
2020-08-29 09:45:39 +0200

    functions & methods

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const startGameBtn = document.getElementById('start-game-btn');
+// jshint esversion: 9
+const startGameBtn = document.getElementById("start-game-btn");
+
+function startGame() {
+  console.log("game is starting");
+}
+
+const person = {
+  name: "sait",
+  greet: function greet() {
+    console.log("hello there");
+  },
+};
+
+console.log(person.name);
+person.greet();
+
+startGameBtn.addEventListener("click", startGame);
